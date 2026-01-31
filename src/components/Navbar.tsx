e8eeff0b79a940df56dadd3253159f79b8295698
Anshori Abdulloh <areknautics@gmail.com>
2026-01-31 11:06:21 +0700

    fix error build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 import { LuHeadset, LuSettings } from "react-icons/lu";
 import { RiUserLine } from "react-icons/ri";
